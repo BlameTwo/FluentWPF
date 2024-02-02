a4e3b83e9ffe618ae36e69194288b54b5ca11499
CiaoName <65759143+BlameTwo@users.noreply.github.com>
2024-02-02 11:39:54 +0800

    增加提示设置

---
+++ FluentWPF/Controls/SettingTipCard.cs
@@ -0,0 +1,57 @@
+﻿using FluentWPF.Common.Bases;
+
+namespace FluentWPF.Controls;
+
+public class SettingTipCard : ControlBase
+{
+    //创建一个枚举类型来区分提示框类型
+
+
+    /// <summary>
+    /// 提示类型
+    /// </summary>
+    public SettingTipType TipType
+    {
+        get { return (SettingTipType)GetValue(TipTypeProperty); }
+        set { SetValue(TipTypeProperty, value); }
+    }
+
+    public static readonly DependencyProperty TipTypeProperty = DependencyProperty.Register(
+        "TipType",
+        typeof(SettingTipType),
+        typeof(SettingTipCard),
+        new PropertyMetadata(SettingTipType.Default)
+    );
+
+    /// <summary>
+    /// 标题
+    /// </summary>
+    public object Header
+    {
+        get { return (object)GetValue(HeaderProperty); }
+        set { SetValue(HeaderProperty, value); }
+    }
+
+    public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+        "Header",
+        typeof(object),
+        typeof(SettingTipCard),
+        new PropertyMetadata("")
+    );
+
+    /// <summary>
+    /// 说明
+    /// </summary>
+    public object Description
+    {
+        get { return (object)GetValue(DescriptionProperty); }
+        set { SetValue(DescriptionProperty, value); }
+    }
+
+    public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+        "Description",
+        typeof(object),
+        typeof(SettingTipCard),
+        new PropertyMetadata(null)
+    );
+}
---
+++ FluentWPF/Models/Enums/SettingTipType.cs
@@ -0,0 +1,24 @@
+﻿namespace FluentWPF.Models.Enums;
+
+/// <summary>
+/// 设置提示卡片枚举
+/// </summary>
+public enum SettingTipType
+{
+    /// <summary>
+    /// 默认
+    /// </summary>
+    Default,
+    /// <summary>
+    /// 正确
+    /// </summary>
+    Success,
+    /// <summary>
+    /// 错误
+    /// </summary>
+    Error,
+    /// <summary>
+    /// 警告
+    /// </summary>
+    Warn
+}
